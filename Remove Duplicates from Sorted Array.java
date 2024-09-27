3cb92ebabba06e4511dd6ec09d29be6de1f07d19
Chaitanya Jadhav <jadhavchaitanya5911@gmail.com>
2024-09-28 00:55:46 +0530

    Update Remove Duplicates from Sorted Array.java

--- Remove Duplicates from Sorted Array.java
+++ Remove Duplicates from Sorted Array.java
@@ -1,3 +1,4 @@
+//LeetCode problem Solution
 class Solution {
     public int removeDuplicates(int[] nums) {
         int uniquecount = 0;
@@ -11,4 +12,20 @@ class Solution {
         
         return uniquecount + 1;  // +1 because uniquecount is index-based, we need element count
     }
+}
+
+// 
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int nums[]={0,0,1,1,1,2,2,3,3,4,5};
+        int uniquecount =0;
+        for(int i=1; i<nums.length; i++){
+            while(nums[i]!=nums[uniquecount]){
+                uniquecount++;
+                nums[uniquecount]=nums[i];
+            }
+           }
+            System.out.println(" "+(uniquecount+1));
+     }
 }
