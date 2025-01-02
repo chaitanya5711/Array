7c57fe0e50c28db97c3e470133ecf1979c57b17c
Chaitanya Jadhav <jadhavchaitanya5911@gmail.com>
2025-01-02 15:15:54 +0530

    Create leaders.java

---
+++ leaders.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]) {
+        int arr[] = {16, 17, 4, 3, 5, 2};
+        
+        // Start from the rightmost element
+        int maxRight = arr[arr.length - 1]; // Initialize with the last element
+        System.out.println(maxRight); // The rightmost element is always a leader
+        
+        // Traverse the array in reverse order
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] >= maxRight) {
+                System.out.println(arr[i]); // If the current element is greater than or equal to maxRight, it's a leader
+                maxRight = arr[i]; // Update maxRight to the current element
+            }
+        }
+    }
+}
